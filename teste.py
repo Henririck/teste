81f603603c78c8279efdc42bf7b091cf293323d2
Henririck <4501531400@estudante.sed.sc.gov.br>
2024-12-03 21:15:07 -0300

    mod: while

---
+++ teste.py
@@ -0,0 +1,10 @@
+num = int(input("Digite um número "))
+trueorfalse = 1
+
+while  trueorfalse >= 1:
+    if num > 10:
+        print('O numero é maior')
+        trueorfalse = 0
+    else:
+        print('O numero é menor')
+        trueorfalse = 1
